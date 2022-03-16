32f9341db155628e22208ac922bcb9c6651e62f9
Iasmina <il8465v@gre.ac.uk>
2022-03-16 16:29:07 -0700

    Scene Changes when introducing name + button

---
+++ Coursework 2 - Games Technology/Assets/Scripts/Editor/SceneSwitch.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SceneSwitch : EditorWindow
+{
+    string text = "";
+    public int index = 0;
+    public string[] options = new string[] { "aaa" };
+
+    [MenuItem("Scene/Scene Window")]
+    static void DisplayWindow()
+    {
+        EditorWindow.GetWindow(typeof(SceneSwitch));
+    }
+
+    void OnGUI()
+    {
+        text = EditorGUILayout.TextField("Name of the scene: ", text);
+
+        if(GUILayout.Button("Switch"))
+        {
+            Debug.Log("clicked");
+
+            //This loads when inserting text 
+            EditorSceneManager.OpenScene("Assets/Scenes/" + text + ".unity");
+
+
+            //POP-UP
+            //index = EditorGUILayout.Popup(index, options);
+            //EditorGUILayout.Popup(0, options);
+
+            //Create a field that allows you to insert buttons with the names of the scenes + Delete buttons
+
+            //SEARCH HOT TO CREATE/DELETE BUTTONS - The name of the button (or another field) will represent the name of the scene - then call the function above
+        }
+    }
+}
